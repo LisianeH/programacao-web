0ac6301f83145cc7ba6a2212bd808a5cfaa41a20
LisianeH <lisiane.h67@gmail.com>
2025-05-09 09:31:13 -0300

    Atividade0

--- atividade03/atividade03.php
+++ atividade03/atividade03.php
@@ -1,10 +1,14 @@
 <?php
 
 function calculaMedia( $numeros ) {
+    if ( empty( $numeros ) ) {
+        return "A lista de números está vazia.";
+    }
+    
     $quantidade = count( $numeros );
     $soma = array_sum( $numeros );
-    $resultado = $soma / $quantidade
-    return $resultado
+    $resultado = $soma / $quantidade;
+    return $resultado;
 }
 
 $numeros = [ 10, 2, 5, 3 ];
